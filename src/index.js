f3e720b1ceb8147ccbb895e76ecd0ac130d69947
DevOS_001 <miguel2official@gmail.com>
2023-09-21 12:23:26 -0500

    env

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const misRutas = require('./rutas/empleados')
-
-
+const misRutas = require("./rutas/empleados");
 
 const app = express();
-const puerto= process.env.PUERTO ||  9000;
-
+const puerto = process.env.PUERTO || 9000;
 
 //---------
 app.use(express.json());
-app.use('/api', misRutas);
-
-
+app.use("/api", misRutas);
 
 //-------------- rutas
-app.get('/', (req, res) =>{
-    res.send('esta es mi api');
-} );
+app.get("/", (req, res) => {
+  res.send("esta es mi api");
+});
 
 //--------------- conexion a mongo
 
 //mongoose.connect(process.env.MONGO_URI).then(() => {console.log('conexion exitosa')}).catch((error) =>{console.error(error)});
-mongoose.connect(process.env.mongo_uri).then(() => {console.log('conexion exitosa')}).catch((error) =>{console.error(error)});
+mongoose
+  .connect(process.env.mongo_uri)
+  .then(() => {
+    console.log("conexion exitosa");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-
-
-
-app.listen(puerto, () => console.log("ejecutandose en el puerto: ", puerto))
+app.listen(puerto, () => console.log("ejecutandose en el puerto: ", puerto));
